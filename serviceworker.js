7e71a89c08caeec8eab7c342e9326507edf0cd3d
MERRIFIELD Christopher A <m026540e@student.staffs.ac.uk>
2017-03-28 10:18:01 +0100

    test

--- serviceworker.js
+++ serviceworker.js
@@ -1,12 +1,46 @@
-var CACHE_NAME = 'gih-cache-v4';
+var CACHE_NAME = 'gih-cache-v5';
 var CACHED_URLS = [
-  'offline.html',
-  'mystyles.css',
-  'dino.png'
+  // Our HTML
+  'first.html',
+  // Stylesheets and fonts
+    'matstyle.css',
+  // JavaScript
+    'mat.js',
+  // Images
+    'appimages/android-icon-36x36.png',
+    'appimages/android-icon-48x48.png',
+    'appimages/android-icon-72x72.png',
+    'appimages/android-icon-96x96.png',
+    'appimages/android-icon-144x144.png',
+    'appimages/android-icon-192x192.png',
+    'appimages/favicon.ico',
+    'appimages/favicon-16x16.png',
+    'appimages/favicon-32x32.png',
+    'appimages/favicon-96x96.png',
+    'appimages/dino.png',
+    'appimages/paddy.png',
+    'appimages/jack.png',
+    'eventsimages/example-blog01.png',
+    'eventsimages/example-blog02.png',
+    'eventsimages/example-blog03.png',
+    'eventsimages/example-blog04.png',
+    'eventsimages/example-blog05.png',
+    'eventsimages/example-blog06.png',
+    'eventsimages/example-blog07.png',
+    'eventsimages/example-work01.png',
+    'eventsimages/example-work02.png',
+    'eventsimages/example-work03.png',
+    'eventsimages/example-work04.png',
+    'eventsimages/example-work05.png',
+    'eventsimages/example-work06.png',
+    'eventsimages/example-work07.png',
+    'eventsimages/example-work08.png',
+    'eventsimages/example-work09.png',
+    
 ];
 
-
 self.addEventListener('install', function(event) {
+  // Cache everything in CACHED_URLS. Installation will fail if something fails to cache
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll(CACHED_URLS);
@@ -21,7 +55,7 @@ self.addEventListener('fetch', function(event) {
         if (response) {
           return response;
         } else if (event.request.headers.get('accept').includes('text/html')) {
-          return caches.match('offline.html');
+          return caches.match('first.html');
         }
       });
     })
@@ -33,7 +67,7 @@ self.addEventListener('activate', function(event) {
     caches.keys().then(function(cacheNames) {
       return Promise.all(
         cacheNames.map(function(cacheName) {
-          if (CACHE_NAME !== cacheName && cacheName.startsWith('gih-cache')) {
+          if (cacheName.startsWith('gih-cache') && CACHE_NAME !== cacheName) {
             return caches.delete(cacheName);
           }
         })
